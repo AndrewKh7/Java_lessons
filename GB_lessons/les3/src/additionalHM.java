534df699ceedad6693b4210b02508ed968a0ded5
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2019-12-20 18:20:44 +0300

    additional HomeWork complete

--- GB_lessons/les3/src/additionalHM.java
+++ GB_lessons/les3/src/additionalHM.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
+
 
 public class additionalHM {
     public static void main(String[] args) {
-        int[][] mat = new int[5][5];
+        int[][] mat = new int[9][9];
         fillingMat(mat);
         for (int[] st : mat){
             for (int c: st )
-                System.out.print(c+" ");
+                System.out.print(c+"\t");
             System.out.println();
         }
     }
@@ -16,12 +16,13 @@ public class additionalHM {
         int I = arr[0].length;
         int S = I*J;
         for (int j = 0; j < J; j++) {
-            for (int i = 0; i < arr[0].length; i++) {
-                int l = min(i, j, I-i-1, J-j-1);
+            for (int i = 0; i < I; i++) {
+//                int l = min(i, j, I-i-1, J-j-1);
+                int l = Math.min(i,Math.min(j,Math.min(I-i-1,J-j-1)));
                 if ( (i==(I-l-1)) || (j==l))
-                    arr[j][i] = i+j-2*l+S-I*(I-2*l)*(J-2*l);
+                    arr[j][i] = i+j - 2*l + S - (I - 2*l) * (J - 2*l);
                 else
-                    arr[j][i] = S-(I-2*(l+1))*(J-2*(l+1)) -i -j + 2*l;
+                    arr[j][i] = S-(I-2*(l+1))*(J-2*(l+1))-i-j+2*l;
             }
         }
     }
@@ -29,10 +30,10 @@ public class additionalHM {
     static int min(int a, int b, int c, int d){
         int min = a;
          if (d < min)
-             min = a;
-         else if (b < min)
+             min = d;
+         if (b < min)
              min = b;
-         else if (c < min)
+         if (c < min)
              min = c;
         return min;
     }
