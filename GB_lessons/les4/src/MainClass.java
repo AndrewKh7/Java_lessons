154df5766388a0e9d60095f62cb4cda809abc18b
Andrey khrapkov <andrey.i.khrapkov@gmail.com>
2020-01-10 12:18:59 +0000

    AI is somehow work

--- GB_lessons/les4/src/MainClass.java
+++ GB_lessons/les4/src/MainClass.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class MainClass {
     static int field_size_x = 3;
@@ -25,36 +23,24 @@ public class MainClass {
         field = initField();
         printField();
         int i = 0;
-        int check_win = 0;
         while( i < field_size_y*field_size_y) {
             if ( i%2 == 0){
                 nextUserTurn();
-                check_win = prewinDot(last_user_point[0], last_user_point[1],USER_DOT);
+
             }else{
-                int ai_check_win = prewinDot(last_ai_point[0], last_ai_point[1],AI_DOT);
-                if (ai_check_win != 0){
-                    if( ai_check_win == 1 && check_win == 1 )
-                        nextAITurn();
-                    else if (ai_check_win > check_win)
-                        setDOT(ai_block_point[0], ai_block_point[1], AI_DOT);
-                    else
-                        setDOT(ai_win_point[0],ai_win_point[1],AI_DOT);
-
-                }else{
-                    check_win = ai_check_win;
-                }
+                nextAITurn();
             }
-
-
             printField();
-            if(check_win == 0) {
-                if ( i%2 == 0) {
+            if( i%2 == 0) {
+                if (checkWin(last_user_point[0], last_user_point[1], USER_DOT)) {
                     System.out.println("User win!");
+                    break;
                 }
-                else {
+            } else {
+                if (checkWin(last_ai_point[0], last_ai_point[1],AI_DOT)){
                     System.out.println("AI win!");
+                    break;
                 }
-                break;
             }
             ++i;
         }
@@ -79,7 +65,7 @@ public class MainClass {
         char[][] temp = new char[field_size_y][field_size_x ];
         for (int i = 0; i < field_size_x ; i++) {
             for (int j = 0; j < field_size_y; j++) {
-               temp[i][j] = '.';
+               temp[j][i] = '.';
             }
         }
         return temp;
@@ -139,10 +125,25 @@ public class MainClass {
     public static void nextAITurn(){
         int x;
         int y;
-        do{
-            x = rand.nextInt(field_size_x);
-            y = rand.nextInt(field_size_y);
-        }while (!setDOT(x,y,AI_DOT));
+        int check_win = prewinDot(last_user_point[0], last_user_point[1],USER_DOT);
+        int ai_check_win = prewinDot(last_ai_point[0], last_ai_point[1],AI_DOT);
+        if (ai_check_win != 0){
+            if( ai_check_win == 1 && check_win == 1 )
+                do{
+                    x = rand.nextInt(field_size_x);
+                    y = rand.nextInt(field_size_y);
+                }while (!setDOT(x,y,AI_DOT));
+            else{
+                if( check_win == 1)
+                    setDOT(ai_win_point[0],ai_win_point[1],AI_DOT);
+                else if ( ai_check_win == 1)
+                    setDOT(ai_block_point[0], ai_block_point[1], AI_DOT);
+                else if ( ai_check_win > check_win )
+                    setDOT(ai_block_point[0], ai_block_point[1], AI_DOT);
+                else
+                    setDOT(ai_win_point[0],ai_win_point[1],AI_DOT);
+            }
+        }
     }
 
     public static boolean checkWin(int x, int y, char dot){
@@ -156,6 +157,46 @@ public class MainClass {
     }
 
     public static int countingDot(int x, int y, int dir, char dot){
+        int cnt = 0;
+        while ( x >= 0 && y >= 0 && x < field_size_x && y <  field_size_y && (field[y][x] == dot) ) {
+            cnt += 1;
+            switch (dir) {
+                case 0:
+                    --x;
+                    break;
+                case 1:
+                    --x;
+                    --y;
+                    break;
+                case 2:
+                    --y;
+                    break;
+                case 3:
+                    --y;
+                    ++x;
+                    break;
+                case 4:
+                    ++x;
+                    break;
+                case 5:
+                    ++x;
+                    ++y;
+                    break;
+                case 6:
+                    ++y;
+                    break;
+                case 7:
+                    ++y;
+                    --x;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+        return cnt;
+    }
+
+    public static int countingDotAndEpmty(int x, int y, int dir, char dot){
         int cnt = 0;
         while ( x >= 0 && y >= 0 && x < field_size_x && y <  field_size_y && (field[y][x] == dot || field[y][x] == '.') ) {
             cnt += 1;
@@ -242,16 +283,17 @@ public class MainClass {
 
     public static int prewinDot(int x, int y, char dot) {
         if(field[y][x] != dot) return 1;
-        int ret_val = 0;
+        int ret_val = 1;
         int up = 0;
         int down = 0;
         int[] step_and_point = new int[3]; // {how many step, point} ????
         int dir;
         int[] arr = null;
+        int[] point = new int[2];
         int steps_and_point = 0;
         for ( dir = 0 ; dir < 4; dir++) {
-            up = countingDot(x, y, dir, dot);
-            down = countingDot(x, y, dir + 4 , dot);
+            up = countingDotAndEpmty(x, y, dir, dot);
+            down = countingDotAndEpmty(x, y, dir + 4 , dot);
 
                 switch (dir) {
                     case 0:
@@ -271,41 +313,33 @@ public class MainClass {
                         break;
                 }
                 point = checkWinLine(arr);
-                if ( point >= 0){
-                     ret_val =  stepAndPoint(point, arr) > 1 ? 3 : 2; //user or ai can win
-                     break;
-                } else if ( point == -1){
-                    ret_val = 1; // nothing
-                    break;
-                } else if (point == -2){
+                if (point[1] == -2){
                     ret_val = 0; // user or ai win
-                }
+                }else if (point[1] >= 0){
+                     ret_val = point[0] + 1;
+                     break;
+                 }
 
             }
         int add_val = 0;
-        if (arr != null && point >= 0) {
-            for (int i = 0; i <= point; i++)
-                add_val += (arr[i] < 0) ? -arr[i] : arr[i];
-            if (steps_and_point == 0 || steps_and_point == 2)
-                add_val += arr[point] - 1;
-
+        if (arr != null && point[1] >= 0) {
             if (dot == 'X')
                 switch (dir) {
                     case 0:
-                        ai_block_point[0] = x - up + add_val;
+                        ai_block_point[0] = x - up + 1 + point[1];
                         ai_block_point[1] = y;
                         break;
                     case 1:
-                        ai_block_point[1] = y - up + add_val;
-                        ai_block_point[0] = x - up + add_val;
+                        ai_block_point[1] = y - up + 1 + point[1];
+                        ai_block_point[0] = x - up + 1 + point[1];
                         break;
                     case 2:
                         ai_block_point[0] = x;
-                        ai_block_point[1] = y - up + add_val;
+                        ai_block_point[1] = y - up + 1 + point[1];
                         break;
                     case 3:
-                        ai_block_point[0] = x + up - add_val;
-                        ai_block_point[1] = y - up + add_val;
+                        ai_block_point[0] = x + up - 1 - point[1];
+                        ai_block_point[1] = y - up + 1 + point[1];
                         break;
                     default:
                         break;
@@ -313,20 +347,20 @@ public class MainClass {
             else
                 switch (dir) {
                     case 0:
-                        ai_win_point[0] = x - up + add_val;
+                        ai_win_point[0] = x - up + 1 + point[1];
                         ai_win_point[1] = y;
                         break;
                     case 1:
-                        ai_win_point[1] = y - up + add_val;
-                        ai_win_point[0] = x - up + add_val;
+                        ai_win_point[1] = y - up + 1 + point[1];
+                        ai_win_point[0] = x - up + 1 + point[1];
                         break;
                     case 2:
                         ai_win_point[0] = x;
-                        ai_win_point[1] = y - up + add_val;
+                        ai_win_point[1] = y - up + 1 + point[1];
                         break;
                     case 3:
-                        ai_win_point[0] = x + up - add_val;
-                        ai_win_point[1] = y - up + add_val;
+                        ai_win_point[0] = x + up - 1 - point[1];
+                        ai_win_point[1] = y - up + 1 + point[1];
                         break;
                     default:
                         break;
@@ -335,46 +369,68 @@ public class MainClass {
         return ret_val;
     }
 
+
+    /*
+    res[0] - how many step to win
+    res[1] - point to block (or win)
+     */
     public static int[] checkWinLine(int[] line) {
         int[] res = {0, -1};
 
         if(line.length == 1 || line[1] == 0) {
-            if (line[0] >= quantity)
+            if (line[0] >= quantity) {
                 res[0] = 0;
                 res[1] = -2;
+            }
         } else {
             for (int i = 0; i < line.length && line[i] != 0; ++i) {
                 if (line[i] >= quantity) {
-
+                    res[0] = 0;
                     res[1] = -2;
                     break;
                 }
                 if (line[i] == quantity - 1) {
                     if (i == line.length - 1 || line[i + 1] == 0) {
-                        res = i - 1;
+                        res[0] = 1;
+                        res[1] = absSumArr(i - 1, line) - 1;
                     }else{
-                        res = i + 1;
+                        res[0] = 1;
+                        res[1] = absSumArr(i, line);
                     }
+                    break;
                 }
                 if (line[i] == -1) {
                     if (i == 0) {
                         if (line[i + 1] == quantity - 1) {
-                            res = i;
+                            res[0] = 1;
+                            res[1] = 0;
                             break;
                         }
                     } else if (i == line.length - 1 || line[i + 1] == 0) {
                         if (line[i - 1] >= quantity - 1) {
-                            res = i;
+                            res[0] = 1;
+                            res[1] = absSumArr(i-1, line);
                             break;
                         }
                     } else if (line[i - 1] + line[i + 1] >= quantity - 1) {
-                        res = i;
+                        res[0] = 1;
+                        res[1] = absSumArr(i-1, line);
+                        break;
+                    }else if ( i > 1 && (i+2< line.length && line[i+2] != 0) && line[i-1] + line[i+1] == quantity -2){
+                        res[0] = 2;
+                        res[1] = absSumArr(i-1,line);
                         break;
                     }
                 }
-                if (line[i] == quantity - 2 && i != 0 && i != line.length - 1) {
+                if (line[i] == quantity - 2 && i != 0 && i != line.length - 1 && line[i+1] != 0) {
                     if (line[i - 1] + line[i + 1] <= -3) {
-                        res = i - 1;
+                        if ( line[i - 1] < line[ i + 1]){
+                            res[0] = 2;
+                            res[1] = absSumArr(i-1, line) - 1;
+                        }else{
+                            res[0] = 2;
+                            res[1] = absSumArr(i, line);
+                        }
                         break;
                     }
                 }
@@ -382,6 +438,14 @@ public class MainClass {
             }
         }
 
+        return res;
+    }
+
+    public static int absSumArr(int i, int[] line){
+        int res = 0 ;
+        for (int j = 0; j <= i; j++) {
+                res += line[j] > 0 ? line[j] : -line[j];
+        }
         return res;
     }
 
