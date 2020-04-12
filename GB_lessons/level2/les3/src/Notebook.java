3fbfb5d172bdf9e83ad2a902969889ef823848d9
Andrey Khrapkov <andrey.i.khrapkov@gmail.com>
2020-04-12 14:35:50 +0300

    level2/les3 main task with notebook complete

--- GB_lessons/level2/les3/src/Notebook.java
+++ GB_lessons/level2/les3/src/Notebook.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Notebook {
+    public static Map<String, ArrayList<String>> notebook = new HashMap<>();
+    public static void main(String[] args){
+        add("Lera","+123");
+        add("Lera", "+321");
+        add("Lera", "+5621");
+        add("Petrov", "+9999324321");
+        add("Petrov", "+090909905621");
+        add("Gada", "+000321");
+        add("Gada", "+111321");
+        add("Gada", "+2222321");
+        add("Gada", "+333321");
+
+        print(get("Lera"));
+        System.out.println("_____________");
+        print(get("Petrov"));
+        System.out.println("_____________");
+        print(get("Gada"));
+    }
+
+    static void add(String surname, String number){
+        if(!notebook.containsKey(surname))
+            notebook.put(surname,new ArrayList<>());
+        notebook.get(surname).add(number);
+    }
+
+    static String[] get(String surname){
+        return  notebook.get(surname).toArray( new String[0]);
+    }
+
+    static void print(String[] num){
+        for (String n: num) {
+            System.out.println(n);
+        }
+    }
 }
